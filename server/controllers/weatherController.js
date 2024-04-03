dc288b31d05202f4c193988ab8f2977bae021a73
marioskam3 <85133854+marioskam3@users.noreply.github.com>
2024-04-03 17:57:32 +0300

    getWeather API req

--- server/controllers/weatherController.js
+++ server/controllers/weatherController.js
@@ -2,34 +2,26 @@ const {appConfig} = require("../config/app-config");
 const axios = require('axios');
 
 const getWeather = async (req, res) => {
-
-
+    const { city } = req.query;
+    //console.log(city);
     try {
-        const { city } = req.query.city;
-        const response= await axios.get(
-            `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${appConfig.weatherApiKey}`)
-            .then((response) => response.json()
-        );
+        const weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${appConfig.weatherApiKey}`);
 
-        if(!response.ok){
+        if (weatherResponse.status !== 200) {
             if (response.status === 404) {
                 throw new Error('Data not found');
-            } else if (response.status === 500) {
+              } else if (response.status === 500) {
                 throw new Error('Server error');
-            } else {
-                throw new Error('Network response was not ok');
-            }
+              } else {
+                throw new Error('Internal server error');
+              }
         }
 
-        console.log(JSON.stringify(response));
-        res.render('index.ejs',{title:'Weather App'});
-        return JSON.stringify(response);
-        
-    
-    }    
-    catch (error) {
+        return res.status(200).json(weatherResponse.data);
+
+    } catch (error) {
         res.status(500).json({ error: error.message });
-    } 
+    }
 }
 
 module.exports={getWeather};
--- server/routes/weatherRoutes.js
+++ server/routes/weatherRoutes.js
@@ -4,42 +4,7 @@ const weatherController=require('../controllers/weatherController.js');
 const axios = require('axios');
 const {appConfig} = require("../config/app-config");
 
-
-
-//router.get('/',weatherController.getWeather);
-
-router.get('/', async (req,res)=>{
-    const { city } = req.query;
-    console.log(city);
-    try {
-        const response= await axios({
-            method: 'GET',
-            url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${appConfig.weatherApiKey}`,
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-
-        if(!response.ok){
-            if (response.status === 404) {
-                throw new Error('Data not found');
-            } else if (response.status === 500) {
-                throw new Error('Server error');
-            } else {
-                throw new Error('Network response was not ok');
-            }
-        }
-
-        console.log(JSON.stringify(response));
-        res.render('index.ejs',{title:'Weather App'});
-        return JSON.stringify(response);
-        
-    
-    }    
-    catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-    
-});
+router.get('/',weatherController.getWeather);
 
 module.exports=router
+
